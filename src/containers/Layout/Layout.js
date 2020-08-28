b1912e56bd887c368a19fe87b985d6b5f5454e88
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-08-27 23:51:31 -0300

    correccion de errores

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import {Switch, Route, Redirect, withRouter} from "react-router-dom";
+import Multas from "../../components/Multas/Multas";
 import Perfil from "../../components/Perfil/Perfil";
 import estilos from "./Layout.module.css";
 import BarraSuperior from "../../components/BarraSuperior/BarraSuperior";
 import { Container, Box } from "@material-ui/core";
+import Multa from "../../components/Multa/Multa";
 import Usuarios from "../../components/Usuarios/Usuarios";
+import Usuario from "../../components/Usuario/Usuario";
 
 class Layout extends Component {
 
@@ -14,9 +17,13 @@ class Layout extends Component {
                 <BarraSuperior />
                 <Container className={estilos.Contenido}>
                     <Switch>
+                        <Route path="/multas/:id" exact={true} component={Multa} />
+                        <Route path="/multas" component={Multas} />
                         <Route path="/perfil" component={Perfil} />
+                        <Route path="/usuarios/:id" exact={true} component={Usuario} />
                         <Route path="/usuarios" component={Usuarios} />
-                        <Redirect from="/" to="/usuarios" />
+                        {/* esto es temporal, esta para probar nomas */}
+                        <Redirect from="/" to="/multas" />
                     </Switch>
                 </Container>
             </Box>
