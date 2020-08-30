6c96f0457f6b17e1c9d26c694850114647b2226a
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-08-29 22:04:43 -0300

    correccion de errores

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import {Switch, Route, Redirect, withRouter} from "react-router-dom";
-import Multas from "../../components/Multas/Multas";
 import Perfil from "../../components/Perfil/Perfil";
 import estilos from "./Layout.module.css";
 import BarraSuperior from "../../components/BarraSuperior/BarraSuperior";
 import { Container, Box } from "@material-ui/core";
-import Multa from "../../components/Multa/Multa";
 import Usuarios from "../../components/Usuarios/Usuarios";
 import Usuario from "../../components/Usuario/Usuario";
 
@@ -17,8 +15,6 @@ class Layout extends Component {
                 <BarraSuperior />
                 <Container className={estilos.Contenido}>
                     <Switch>
-                        <Route path="/multas/:id" exact={true} component={Multa} />
-                        <Route path="/multas" component={Multas} />
                         <Route path="/perfil" component={Perfil} />
                         <Route path="/usuarios/:id" exact={true} component={Usuario} />
                         <Route path="/usuarios" component={Usuarios} />
