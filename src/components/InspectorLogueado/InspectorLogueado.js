11d833a819161b31ea3443aeaec1d978b746fc90
Beto-23 <mansilla_alberto@outlook.com>
2020-11-22 17:50:21 -0300

    Centered Not Allowed Message

--- src/components/InspectorLogueado/InspectorLogueado.js
+++ src/components/InspectorLogueado/InspectorLogueado.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import { Container, Typography } from "@material-ui/core";
+import estilos from "./InspectorLogueado.module.css";
+
 
 class InspectorLogueado extends Component {
     render() {
         return (
-            <Container>
-                <Typography variant="h3">Utilice la aplicación móvil de MultApp</Typography>
-                <Typography variant="h5">La aplicación web es sólo para Supervisores y Administradores</Typography>
+            <Container className={estilos.contenedor}>
+                <div className={estilos.titulos}>
+                    <Typography align="center" variant="h3">
+                        Utilice la aplicación móvil de MultApp
+                    </Typography>
+                    <Typography align="center" variant="h5">
+                        La aplicación web es sólo para Supervisores y Administradores
+                    </Typography>
+                </div>
             </Container>
         );
     }
